7044fab6dadddc63927a318116eae460f69d2a19
Антон <toshiksokil@mail.ru>
2016-04-30 16:27:38 +0300

    threads

--- src/ClassThread.java
+++ src/ClassThread.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClassThread{
+
     public static void main(String[] args) throws InterruptedException{
         List<MyThread> threads = new ArrayList<>();
 
